89129d9ab1feef5fc15f4daab52351b647898758
Alexandru Popitan <alexandru.popitan@assist.ro>
2018-11-28 15:43:11 +0200

    Added Landing screen

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
-import {Router, Stack, Scene} from 'react-native-router-flux';
+import React, { Component } from 'react';
+import { Router, Stack, Scene } from 'react-native-router-flux';
 import LoginScreen from './src/screens/login/loginScreen';
 import MainScreen from './src/screens/main/mainScreen';
+import LandingScreen from './src/screens/landing/landingScreen'
 import store from './src/app/store';
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
 import './config/Reactotron';
 
 export default class App extends Component {
@@ -12,8 +13,9 @@ export default class App extends Component {
             <Provider store={store}>
                 <Router>
                     <Stack key="root">
-                        <Scene key="login" component={LoginScreen} title="Login" initial/>
-                        <Scene key="main" component={MainScreen} title="Main"/>
+                        <Scene key="login" component={LoginScreen} title="Login" initial />
+                        <Scene key="main" component={MainScreen} title="Main" />
+                        <Scene key="landing" component={LandingScreen} title="Landing" />
                     </Stack>
                 </Router>
             </Provider>
---
+++ src/screens/landing/landingScreen.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image, StatusBar, TouchableHighlight } from 'react-native';
+
+class LandingScreen extends Component {
+
+    static navigationOptions = {
+        header: null
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar translucent={true} backgroundColor={'transparent'} />
+                <Image source={require('../../assets/rugby.jpg')} style={styles.backgroundImage} />
+                <View style={styles.backgroundCover} />
+                <View style={styles.loginForm}>
+                    <View style={{ alignItems: 'center', }}>
+                        <Text style={styles.titleStyle}>ROTOLEGENDS</Text>
+                        <Text style={styles.subTitleStyle}>FANTASY NEWS</Text>
+                    </View>
+                    <View style={{ position: 'absolute', bottom: 0, flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+                        <Text style={styles.loginWithStyle}>Login with</Text>
+                        <View style={{ width: '100%', flexDirection: 'row', justifyContent: 'space-around' }}>
+                            <TouchableHighlight onPress={this._onPressFacebookButton} style={styles.circleContainer} >
+                                <Image
+                                    style={styles.imageStyle}
+                                    source={require('../../assets/facebook.png')}
+                                />
+                            </TouchableHighlight>
+                            <TouchableHighlight onPress={this._onPressLoginButton} style={styles.circleContainer} >
+                                <Image
+                                    style={styles.imageStyle}
+                                    source={require('../../assets/email.png')}
+                                />
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+
+    _onPressFacebookButton() {
+        // TODO, implement login with Facebook account
+    }
+
+    _onPressLoginButton() {
+        // TODO, show login screen
+    }
+}
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    backgroundImage: {
+        flex: 1,
+        width: '100%',
+        height: '100%'
+    },
+    backgroundCover: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        position: 'absolute'
+    },
+    loginForm: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        alignItems: 'center',
+        justifyContent: 'center',
+        right: 0
+    },
+    titleStyle: {
+        fontSize: 54,
+        fontWeight: 'bold',
+        color: 'white',
+
+    },
+    subTitleStyle: {
+        fontSize: 15,
+        color: 'white'
+    },
+    loginWithStyle: {
+        fontSize: 26,
+        color: 'white',
+        fontWeight: 'bold',
+        alignItems: 'center',
+        flex: 1,
+        alignSelf: 'center'
+    },
+    imageStyle: {
+        backgroundColor: 'transparent',
+        height: 40,
+        width: 40,
+        borderRadius: 3,
+    },
+    circleContainer: {
+        width: 90,
+        height: 90,
+        borderRadius: 90 / 2,
+        backgroundColor: 'transparent',
+        borderColor: 'white',
+        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 30,
+        marginTop: 25,
+    }
+});
+
+export default connect()(LandingScreen);
