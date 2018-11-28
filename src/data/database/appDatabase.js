fbf3d695512a1bcabbda129aeb2794f24f026865
Iura <iura.gaitur@gmail.com>
2018-11-28 11:50:49 +0200

    Added user repository and db access

---
+++ src/data/database/appDatabase.js
@@ -0,0 +1,6 @@
+import PouchDB from 'pouchdb-react-native'
+PouchDB.plugin(require('pouchdb-adapter-asyncstorage').default);
+PouchDB.plugin(require('pouchdb-find'));
+const AppDatabase = new PouchDB('Sporter', {adapter: 'asyncstorage'});
+
+export default AppDatabase;
---
+++ src/data/database/userRepository.js
@@ -0,0 +1,23 @@
+import {AsyncStorage} from 'react-native';
+
+export default class UserRepository {
+
+    PRIMARY_USER_KEY = 'USER_KEY';
+
+    async savePrimaryUser(user) {
+        if(!user) return;
+        return await AsyncStorage.setItem(this.PRIMARY_USER_KEY, JSON.stringify(user));
+    }
+
+    async getPrimaryUser() {
+        let data = await AsyncStorage.getItem(this.PRIMARY_USER_KEY);
+        if (data) {
+            data = JSON.parse(data);
+        }
+        return data;
+    }
+
+    async removePrimaryUser() {
+        return await AsyncStorage.removeItem(this.PRIMARY_USER_KEY);
+    }
+}
