c8101357b8c8dbeaab5de4292d09c32ddc3404ae
alexmaca65 <alexmaca65@yahoo.com>
2018-11-29 16:05:15 +0200

    Integrated Screens

--- src/screens/authentication/authenticationScreen.js
+++ src/screens/authentication/authenticationScreen.js
@@ -65,6 +65,10 @@ class LoginScreen extends Component {
                             onTabPress = { this.indexChange }
                         />
                     </View>
+                    <View style = { styles.bottomView }>
+                       { this.state.selectedIndex == 0 && <LoginView actionLogin = { this.login }/>} 
+                       { this.state.selectedIndex == 1 && <RegisterView actionRegister = { this.register }/>}
+                    </View>
                 </View>
             </View>
         );
@@ -139,7 +143,8 @@ const styles = StyleSheet.create({
     },
     middleView: {
         height: 35,
-        width: '70%'
+        width: '70%',
+        marginTop: 30
     },
     defaultTab: {
         backgroundColor: 'transparent',
@@ -158,6 +163,11 @@ const styles = StyleSheet.create({
         height: '100%',
         position: 'absolute',
         backgroundColor: 'rgba(0,0,0, 0.3)'
+    },
+    bottomView: {
+        marginTop: 60,
+        width: '90%',
+        height: 'auto'
     }
 })
 
